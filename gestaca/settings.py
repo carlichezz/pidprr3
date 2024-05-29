725544eeab9b64daf4c0f019493c10654fc59ea4
mcomyl <carielsa@estudiantes.uci.cu>
2024-05-29 00:27:02 -0400

    Agregado dj-database-url a requierements/base.txt

--- gestaca/settings.py
+++ gestaca/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,7 +82,6 @@ WSGI_APPLICATION = 'gestaca.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
